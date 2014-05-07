54a1e52e06333847d06b5408e203e1843fa5dd7f
Cid Dennis <cid.dennis@gmail.com>
2014-05-07 14:05:01 -0600

    Deep Ostruct

--- lib/elastic/rebound/result.rb
+++ lib/elastic/rebound/result.rb
@@ -1,5 +1,28 @@
 module Elastic
   module Rebound
+    require 'ostruct'
+
+    class DeepStruct < OpenStruct
+      def initialize(hash=nil)
+        @table = {}
+        @hash_table = {}
+
+        if hash
+          hash.each do |k,v|
+            @table[k.to_sym] = (v.is_a?(Hash) ? self.class.new(v) : v)
+            @hash_table[k.to_sym] = v
+
+            new_ostruct_member(k)
+          end
+        end
+      end
+
+      def to_h
+        @hash_table
+      end
+
+    end
+
     class Result
       # require 'hashie'
 
@@ -11,7 +34,7 @@ module Elastic
       end
 
       def hit=(result)
-        @hit = OpenStruct.new(result)
+        @hit = DeepStruct.new(result)
       end
 
       # Retrieve the results from elastic search.
--- lib/elastic/rebound/version.rb
+++ lib/elastic/rebound/version.rb
@@ -1,5 +1,5 @@
 module Elastic
   module Rebound
-    VERSION = "0.2.5"
+    VERSION = "0.2.6"
   end
 end
